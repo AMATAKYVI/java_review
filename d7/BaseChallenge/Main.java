68c2dec6420981d330f66f82f2a3102e39940be0
Amatak Yvi <yviamatak123@gmail.com>
2026-02-16 20:25:55 -0500

    unmodifiable-view only

--- d7/BaseChallenge/Main.java
+++ d7/BaseChallenge/Main.java
@@ -1,6 +1,17 @@
 package d7.BaseChallenge;
 
+import java.util.Arrays;
 import java.util.List;
+
+record Person(String name, String dob, Person[] kids) {
+
+    public Person[] getKids() {
+        for (Person kid : kids) {
+            System.out.println("------ " + kid);
+        }
+        return kids;
+    }
+}
 
 public class Main {
 
@@ -24,6 +35,31 @@ public class Main {
 
         for (BankAccount account : accounts) {
             System.out.println(account);
+        }
+
+        Person jame = new Person("Jame", "2000-01-01", null);
+        Person jame2 = new Person("Jame 2", "2000-01-01", null);
+        Person emma = new Person("Emma", "2000-01-01", null);
+        Person tony = new Person("Tony", "2000-01-01", new Person[]{jame, jame2, emma});
+        Person tom = new Person("Tom", "2000-01-01", null);
+
+        Person[] persons = {jame, jame2, emma, tony, tom};
+
+        Person[] personsCopy = Arrays.copyOf(persons, persons.length);
+
+        Person[] personsCopyDeep = new Person[5];
+
+        for (int i = 0; i < personsCopyDeep.length; i++) {
+            personsCopyDeep[i] = new Person(persons[i].name(), persons[i].dob(), persons[i].kids());
+        }
+
+        for (Person person : personsCopy) {
+            System.out.println(person);
+
+            if (person.kids() != null) {
+                System.out.println("this is the kids:");
+                person.getKids();
+            }
         }
     }
 
